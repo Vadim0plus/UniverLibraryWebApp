9d904d95bd8a847534a226f7b17161444b8aa8f0
Vadim Tukhvatullin <vadim0plus@gmail.com>
2017-08-28 22:40:34 +0300

    First commit

---
+++ src/main/java/com/univerlib/main/App.java
@@ -0,0 +1,13 @@
+package com.univerlib.main;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
